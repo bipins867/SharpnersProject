0fc089215bef52c331d19dab1392e1e6b8044b26
= <=>
2023-09-01 20:00:32 +0530

    Adding Delete

--- routes/todos.js
+++ routes/todos.js
@@ -3,11 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const todos = [];
 const router = (0, express_1.Router)();
-router.get('/', (res, res, next) => {
+router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post('/todo', (req, res, next) => {
     const newTodo = { id: new Date().toISOString(), text: req.body.text };
     todos.push(newTodo);
+});
+router.put('/todo/:todoId', (req, res, next) => {
+    const tid = req.params.todoId;
+    const todoIndex = todos.findIndex(todoItem => todoItem.id === tid);
+    if (todoIndex >= 0) {
+        todos[todoIndex] = { id: tid, text: req.body.text };
+        return res.status(200).json({ message: "Updated" });
+    }
 });
 exports.default = router;
--- routes/todos.ts
+++ routes/todos.ts
@@ -6,7 +6,7 @@ const todos:Todo[]=[];
 
 const router=Router();
 
-router.get('/',(res:Request,res:Response,next)=>{
+router.get('/',(req:Request,res:Response,next)=>{
     res.status(200).json({todos:todos})
 })
 
@@ -14,6 +14,17 @@ router.post('/todo',(req,res,next)=>{
     const newTodo:Todo={id:new Date().toISOString(),text:req.body.text};
     todos.push(newTodo);
 })
+
+
+router.put('/todo/:todoId',(req,res,next)=>{
+    const tid=req.params.todoId;
+    const todoIndex=todos.findIndex(todoItem=>todoItem.id===tid)
+    if(todoIndex>=0){
+        todos[todoIndex]={id:tid,text:req.body.text};
+        return res.status(200).json({message:"Updated"})
+    }
+})
+
 
 export default router;
 
